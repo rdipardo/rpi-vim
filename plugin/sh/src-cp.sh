523feffcb82c059a85689bfd0563dba392926bb4
Robert Di Pardo <dipardo.r@gmail.com>
2023-02-04 13:18:24 -0500

    Optimize file transfer times

--- plugin/sh/src-cp.sh
+++ plugin/sh/src-cp.sh
@@ -2,10 +2,11 @@
 [ -f .env ] && eval "$(cat .env)"
 [ "$SSH_PORT" ] && PORT="$SSH_PORT" || PORT=5022
 [ "$SSH_HOST" ] && SRV="$SSH_HOST" || SRV='pi@localhost'
-[ "$WORK_DIR" ] && WORK_DIR="$WORK_DIR" || WORK_DIR='~'
+[ "$2" = 'all' ] && RECURSIVE='-r --del' && SRC="$(dirname "$1")/" || SRC="$1"
+SRC_NAME="$(basename $SRC)" && [ -n "$RECURSIVE" ] && SRC_NAME="$SRC_NAME/*"
 
-echo "Sending $(basename "$1") to $SRV . . ."
-ssh $SSH_PARAMS -Tq -p "$PORT" "$SRV" "mkdir -p $WORK_DIR" 2>/dev/null
+echo "Sending $SRC_NAME to $SRV . . ."
 set -e
-rsync -e="ssh -p$PORT" "$1" "$SRV:$WORK_DIR/$(basename "$(dirname "$1")")/"
+rsync --exclude-from="$(dirname "$0")/.rsyncignore" --exclude="$(basename "${1%%.*}")" \
+  -u $RECURSIVE --port $PORT "$SRC" "$SRV:$WORK_DIR/$(basename "$(dirname "$1")")/"
 echo 'done'
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -e
 [ -f .env ] && eval "$(cat .env)"
 [ "$SSH_PORT" ] && PORT="$SSH_PORT" || PORT=5022
 [ "$SSH_HOST" ] && SRV="$SSH_HOST" || SRV='pi@localhost'
+[ "$WORK_DIR" ] && WORK_DIR="$WORK_DIR" || WORK_DIR='~'
 KEY_FILE=~/.ssh/rpi_vm
 
 if ! [ -f "$KEY_FILE" ];then
@@ -13,6 +14,11 @@ Host ${SRV#*@}
   IdentityFile $KEY_FILE
   Port $PORT
   User ${SRV%@*}
+  AddressFamily inet
+  Protocol 2
+  ControlMaster auto
+  ControlPath  /tmp/%r@%h-%p
+# PreferredAuthentications publickey
 EOF
 ) >> ~/.ssh/config
 fi
@@ -21,4 +27,6 @@ ssh-copy-id -i "${KEY_FILE}.pub" -p$PORT $SRV
 touch .hushlogin # https://stackoverflow.com/a/3763539
 rsync -v -e="ssh -p$PORT" .hushlogin "$SRV:~"
 rsync -v -e="ssh -p$PORT" ./bin/* "$SRV:~/bin/"
+# shellcheck disable=SC2086
+ssh $SSH_PARAMS -Tq -p "$PORT" "$SRV" "mkdir -p $WORK_DIR"
 rm .hushlogin
